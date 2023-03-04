6fde54a82f0c739a2db5d6160938c65268991667
pinegin pavel <pavel.pinegin.essen@gmail.com>
2023-03-04 18:56:02 +0100

    main+

--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+#include "parser.hpp"
+#include <string>
+
 int main()
 {
+    for (std::string line; std::getline(std::cin, line);) {
+        if(line.empty()) {
+            std::cout << std::endl;
+        } else {
+            auto node = mercury::Parse(line);
+            if(node != nullptr) {
+                std::cout << node->Calc() << std::endl;
+            } else {
+                std::cout << "Invalid input!" << std::endl;
+            }
+        }
+    }
+
     return 0;
 }
